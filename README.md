110cb940b2da9919dfb2d22461c23714e37bdf00
George Gorak <73673803+JurGOn01@users.noreply.github.com>
2024-04-06 10:56:13 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# LoadAS - saving the open software windows setup for later loading
+#### Video Demo:  <URL HERE>
+#### Description:
+TODO
---
+++ test_project.py
@@ -0,0 +1,10 @@
+'''
+test_project.py
+
+will include tests later.
+
+Your test functions must be in a file called test_project.py, which should also be in the “root” of your project. 
+Be sure they have the same name as your custom functions, prepended with test_ 
+(test_custom_function, for example, where custom_function is a function you’ve implemented in project.py).
+
+'''
