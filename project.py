d060398e6ec11bd476eccc607f6857cfbdfad015
George Gorak <73673803+JurGOn01@users.noreply.github.com>
2024-04-14 19:39:01 +0100

    Add flatpak omitting feature

--- project.py
+++ project.py
@@ -3,14 +3,16 @@ logger = logging.getLogger(__name__)
 import subprocess
 import re
 import os
+import sys
 
 class User_App:
-    def __init__(self, PID, x_offset, y_offset, width, height):
+    def __init__(self, PID, x_offset, y_offset, width, height,title):
         self.PID = int(PID)
         self.x_offset = int(x_offset)
         self.y_offset = int(y_offset)
         self.width = int(width)
         self.height = int(height)
+        self.title = title
 
     def __str__(self):
         return f"PID: {self.PID}, x_offset: {self.x_offset},y_offset: {self.y_offset},width: {self.width},height: {self.height}"
@@ -51,7 +53,6 @@ class User_App:
     @property
     def width(self):
         return self._width
-    
     @width.setter
     def width(self,width):
         try:
@@ -63,7 +64,6 @@ class User_App:
     @property
     def height(self):
         return self._height
-
     @height.setter
     def height(self,height):
         try:
@@ -71,6 +71,15 @@ class User_App:
         except TypeError:
             raise TypeError("PID must be integer string or integer")
         self._height = height
+        
+    @property
+    def title(self):
+        return self._title
+    @title.setter
+    def title(self,title):
+        self._title = title
+
+
 
 def main():
     logging.basicConfig(filename="myapp.log", level=logging.INFO)
@@ -93,6 +102,7 @@ def get_active_apps_info():
         logger.info("opened apps_info_temp.txt")
         for line in file:
             app = generate_app_info(line)
+            logger.info(app)
             app_list.append(app)
 
     #return the relevent captured data (dic or list)
@@ -104,7 +114,7 @@ def generate_app_info(s):
     # created with help of: https://regex101.com/r/Op2GDF/1
         # capture relevent information (dic or list) - re
         logger.info("pattern matched in apps_info_temp.txt")
-        temp_user_App = User_App(info.group(3),info.group(4),info.group(5),info.group(6),info.group(7))
+        temp_user_App = User_App(info.group(3),info.group(4),info.group(5),info.group(6),info.group(7),info.group(9))
     else:
         raise LookupError("str does not match rexp:\n\n" + str + "should match regular expression:\n\n ^(0x[0-9A-Fa-f]{8})\s{1,2}(-\d|\d)\s(\d{1,})\s{1,}(\d{1,}|-\d{1,})\s{1,}(\d{1,}|-\d{1,})\s{1,}(\d{3,4})\s{1,4}(\d{3,4})\s{1,4}([A-Za-z0-9-]{1,})\s(.{1,})$")
     return temp_user_App
@@ -117,12 +127,12 @@ def save_apps_2_setup_file(list,filename="session"):
                 file.write(f"{processed_run_command(run_command)}")
     with open(f"{filename}.sup", "w") as file: # .sup - (short for save user programs) plain text format containing commands of apps to load by LoadAS. This file/s with .sup format is/are produced by the user.
         for app in list:
-            run_command = subprocess.check_output(f"ps -p {app.PID} -o cmd=",shell=True,text=True)
-            if int(app.PID) < 100:
-                print("Beware, open flatpack software going to be ignored and it won't included session file. (it won't loaded or open!)")
-                continue # skip/stop from writing the flatpack to the file!
-            logger.info(f"The PID:{app.PID}, is {run_command} command")
-            file.write(f"{processed_run_command(run_command)}")
+            try:
+                run_command = subprocess.check_output(f"ps -p {app.PID} -o cmd=",shell=True,text=True)
+                logger.info(f"The PID:{app.PID}, is {run_command} command")
+                file.write(f"{processed_run_command(run_command)}")
+            except subprocess.CalledProcessError:
+                print(f"Flatpak applications are not supported, {app.title} won't be added to a session file (won't be ran/loaded)")
 
 def processed_run_command(cmd_path):
     if "gjs" not in cmd_path: #avoids gnome-shell 
